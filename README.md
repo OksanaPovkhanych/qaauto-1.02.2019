6f4cd49691492a998b5d6265dffe6a73f0bdb980
Oksana Povkhanych <oknevskaya@gmail.com>
2019-02-08 20:56:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Steps for opening project
+- Install JDK by the link https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html.
+- Download https://www.jetbrains.com/idea/ and install Intellij IDEA.
+- Download https://chromedriver.storage.googleapis.com/index.html?path=2.46/ and copy file to the directory C:\Windows\System32
+- Import project to the Intellij IDEA.
+
---
+++ src/main/java/BadCodeExample.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BadCodeExample {
+    public static void main(String[] args) {
+
+            System.out.println("Hello world!");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.google.com");
+            String searchTerm = "Selenium";
+            WebElement searchfield =  driver.findElement(By.xpath("//input[@name='q']"));
+            searchfield.sendKeys(searchTerm);
+            searchfield.sendKeys(Keys.ENTER);
+            List<WebElement> searchResultElements = driver.findElements(By.xpath("//div[@class='srg']/div"));
+            System.out.println("Search results count: " + searchResultElements.size());
+
+            //For each WebElement in searchResultElements print text
+        for (WebElement searchResultElement : searchResultElements) {
+            String searchResultElementText = searchResultElement.getText();
+            System.out.println(searchResultElementText);
+            if (searchResultElementText.toLowerCase().contains(searchTerm.toLowerCase()))
+            {
+                System.out.println("searchTerm found");
+            }
+            else
+            {
+                System.out.println("searchTerm not found");
+            }
+            System.out.println();
+        }
+
+
+            driver.quit();
+    }
+}
