7a61c57f072e3c75fc608583a1381183ff017be6
Oksana Povkhanych <oknevskaya@gmail.com>
2019-02-19 21:32:57 +0200

    Lesson 6 Hometask 5

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,43 +1,55 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
     @Test
     public void negativeLoginTestWithEmptyPassword() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page did not load after first call");
 
         landingPage.login("a@b.c", "");
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page did not load after login with empty password");
-
-        driver.close();
     }
 
-    @Test
-    public void successfulLoginTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
+    @DataProvider
+    public Object[][] validData() {
+        return new Object[][]{
+                { "k.s.e.n.i.y.a@meta.ua", "test@1989" },
+                { "K.S.E.N.I.Y.A@meta.ua", "test@1989" },
+                { " k.s.e.n.i.y.a@meta.ua", "test@1989" }
+        };
+    }
 
+    @Test(dataProvider = "validData")
+    public void successfulLoginTest(String userEmail, String userPassword) {
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page did not load after first call");
 
-        landingPage.login("k.s.e.n.i.y.a@meta.ua", "test@1989");
+        landingPage.login(userEmail, userPassword);
         HomePage homePage = new HomePage(driver);
         Assert.assertTrue(homePage.isPageLoaded(), "Home page did not load after login to site");
-
-        driver.close();
     }
 
     @Test
     public void negativeLoginWithWrongPassword() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page did not load after first call");
 
@@ -47,14 +59,10 @@ public class LoginTests {
         Assert.assertEquals(loginSubmitPage.getPasswordValidationMessageText(),
                 "Hmm, that's not the right password. Please try again or request a new one.",
                 "Wrong validation message for password field.");
-        driver.close();
     }
 
     @Test
     public void negativeLoginWithWrongEmail() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page did not load after first call");
 
@@ -64,15 +72,10 @@ public class LoginTests {
         Assert.assertEquals(loginSubmitPage.getEmailValidationMessageText(),
                 "Hmm, we don't recognize that email. Please try again.",
                 "Wrong validation message for email field.");
-
-        driver.quit();
     }
 
     @Test
     public void negativeLoginWithIncorrectFormatEmail() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
-
         LandingPage landingPage = new LandingPage(driver);
         Assert.assertTrue(landingPage.isPageLoaded(), "Landing page did not load after first call");
 
@@ -82,7 +85,5 @@ public class LoginTests {
         Assert.assertEquals(loginSubmitPage.getEmailValidationMessageText(),
                 "Please enter a valid email address.",
                 "Wrong validation message for email field.");
-
-        driver.quit();
     }
 }
